b911c43abe2f640dccef373759b00bc8632f827a
Anne <a.heimes93@web.de>
2017-01-19 01:24:00 +0100

    baken gefixt

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -12,6 +12,63 @@ class PeopleController extends AppController
 {
 
     /**
+     * Provides ListFilter configuration 
+     * 
+     * @return array 
+     */ 
+    public function getListFilters() 
+    { 
+        $filters = []; 
+        if ($this->request->action == 'index') { 
+            $filters['fields'] = [ 
+                'People.role' => [ 
+                    'searchType' => 'select', 
+                    'options' => Person::getRoles(), 
+                    'inputOptions' => [ 
+                        'label' => __('user.role') 
+                    ] 
+                ], 
+                'People.status' => [ 
+                    'searchType' => 'select', 
+                    'options' => Person::getStatuses(), 
+                    'inputOptions' => [ 
+                        'label' => __('user.status') 
+                    ] 
+                ], 
+                'People.matriculation_number' => [ 
+                    'searchType' => 'wildcard', 
+                    'inputOptions' => [ 
+                        'label' => __('user.matriculation_number') 
+                    ] 
+                ], 
+                'People.studentenwerk_identification' => [ 
+                    'searchType' => 'wildcard', 
+                    'inputOptions' => [ 
+                        'label' => __('user.studentenwerk_identification') 
+                    ] 
+                ], 
+                'People.status' => [ 
+                    'searchType' => 'select', 
+                    'options' => Person::getStatuses(), 
+                    'inputOptions' => [ 
+                        'label' => __('user.status') 
+                    ] 
+                ], 
+                'People.fulltext' => [ 
+                    'searchType' => 'fulltext', 
+                    'searchFields' => [ 
+                        'People.forename', 
+                        'People.surname', 
+                        'People.email' 
+                    ] 
+                ] 
+            ]; 
+        } 
+ 
+        return $filters; 
+    } 
+ 
+    /** 
      * Index method
      *
      * @return \Cake\Network\Response|null
--- src/Model/Entity/Person.php
+++ src/Model/Entity/Person.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
@@ -34,8 +35,7 @@ use Cake\ORM\Entity;
  * @property \App\Model\Entity\Roomkey[] $roomkeys
  * @property \App\Model\Entity\WorkingGroup[] $working_groups
  */
-class Person extends Entity
-{
+class Person extends Entity {
 
     /**
      * Fields that can be mass assigned using newEntity() or patchEntity().
@@ -59,4 +59,100 @@ class Person extends Entity
     protected $_hidden = [
         'password'
     ];
+
+    /**
+     * Define type descriptions 
+     * 
+     * @return array 
+     */
+    public static function typeDescriptions() {
+        return [
+            self::ROLE_ADMIN => __('user.role.admin'),
+            self::ROLE_NETWORK => __('user.role.network'),
+            self::ROLE_HOUSE_REPRESENTATIVE => __('user.role.house_representative'),
+            self::ROLE_WORKING_GROUP_REPRESENTATIVE => __('user.role.working_group_representative'),
+            self::ROLE_TENANT => __('user.role.tenant'),
+            self::ROLE_ASSIGNMENT_COMMITTEE => __('user.role.assignment_committee'),
+            self::ROLE_HOUSEKEEPER => __('user.role.housekeeper'),
+        ];
+    }
+
+    /**
+     * Returns a map of possible roles for a user with descriptions 
+     * 
+     * @return array 
+     */
+    public static function getRoles() {
+        return self::getTypeMap(
+                        self::ROLE_ADMIN, self::ROLE_NETWORK, self::ROLE_ASSIGNMENT_COMMITTEE, self::ROLE_WORKING_GROUP_REPRESENTATIVE, self::ROLE_TENANT, self::ROLE_HOUSE_REPRESENTATIVE, self::ROLE_HOUSEKEEPER
+        );
+    }
+
+    /**
+     * Returns a map of possible statuses for a user with descriptions 
+     * 
+     * @return array 
+     */
+    public static function getStatuses() {
+        return Status::getMap(Status::ACTIVE, Status::SUSPENDED, Status::DELETED);
+    }
+
+    /**
+     * Setter for hashed password 
+     * 
+     * @param string $password not hashed password 
+     * @return string 
+     */
+    protected function _setPassword($password) {
+        if (!empty($password)) {
+            $password = (new DefaultPasswordHasher)->hash($password);
+        }
+
+        return $password;
+    }
+
+    /**
+     * Getter for the full name 
+     * 
+     * @return string 
+     */
+    protected function _getFullName() {
+        return $this->forename . ' ' . $this->surname;
+    }
+
+    /**
+     * Getter for all roles 
+     * 
+     * @return string 
+     */
+    protected function _getRoles() {
+        $roles = [];
+        foreach ($this->user_roles as $role) {
+            array_push($roles, $role['id']);
+        }
+        return $roles;
+    }
+
+    //TODO this is a hack to bypass the authentication in a simple way. Should be replaced with multi-role support 
+    protected function _getRole() {
+        return Person::ROLE_ADMIN;
+    }
+
+    /**
+     * Api transform for user 
+     * 
+     * @return array 
+     */
+    public function apiTransform() {
+        return [
+            'id' => $this->id,
+            'api_token' => $this->api_token,
+            'status' => $this->status,
+            'role' => $this->role,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'email' => $this->email,
+        ];
+    }
+
 }
